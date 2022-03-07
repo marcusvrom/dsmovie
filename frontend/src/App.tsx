ce61300755e70e14c82adf0f3946a8bb5184ff33
Marcus Romano <marcusvromano02@gmail.com>
2022-03-07 13:26:05 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import {ReactComponent as GithubIcon} from './assets/img/github.svg'
 
 function App() {
   return (
-   <h1>Página principal</h1>
+    <header>
+      <nav className="container">
+          <div className="dsmovie-nav-content">
+              <h1>DSMovie</h1>
+              <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                  <div className="dsmovie-contact-container">
+                      <GithubIcon />
+                      <p className="dsmovie-contact-link">/devsuperior</p>
+                  </div>
+              </a>
+          </div>
+      </nav>
+    </header>
   );
 }
 
