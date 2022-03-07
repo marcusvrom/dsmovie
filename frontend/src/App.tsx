1befd9083707926d3053e32ccb5a8494527c59f8
Marcus Romano <marcusvromano02@gmail.com>
2022-03-07 13:29:56 -0300

    Navbar (Correção de Component)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,8 @@
-import {ReactComponent as GithubIcon} from './assets/img/github.svg'
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <header>
-      <nav className="container">
-          <div className="dsmovie-nav-content">
-              <h1>DSMovie</h1>
-              <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
-                  <div className="dsmovie-contact-container">
-                      <GithubIcon />
-                      <p className="dsmovie-contact-link">/devsuperior</p>
-                  </div>
-              </a>
-          </div>
-      </nav>
-    </header>
+    <Navbar />
   );
 }
 
