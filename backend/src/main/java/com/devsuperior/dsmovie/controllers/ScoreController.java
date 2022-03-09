baef7261753b7253499352cbca4b0f1108b5ed69
Marcus Romano <marcusvromano02@gmail.com>
2022-03-09 12:51:40 -0300

    Save Score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsmovie.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.services.ScoreService;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+	
+	@Autowired
+	private ScoreService scoreService;
+	
+	@PutMapping
+	public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
+		return scoreService.saveScore(dto);
+	}
+}
--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -17,6 +17,10 @@ public class Score {
 	public void setMovie(Movie movie) {
 		id.setMovie(movie);
 	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
 
 	public ScorePK getId() {
 		return id;
